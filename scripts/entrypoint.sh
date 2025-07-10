a27a7286af426b0b3c30a1c0aa4f90150e3e8e12
manh-lam-tpv-clv <manh.lam.tpv@one-line.com>
2025-07-10 15:06:37 +0700

    fix: script to deploy

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,7 @@ function apply_path {
   envs="${!NEXT_@}"
 
   for matched_env in $envs; do
-    test -n "${!matched_env}" && find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i  "s#APP_$matched_env#${!matched_env}#g"
+    test -n "${!matched_env}" && find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i  "s#$matched_env#${!matched_env}#g"
   done
 }
 
